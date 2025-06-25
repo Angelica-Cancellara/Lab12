c642c4b1099429ceb7d91f9a0c9a03825e4df175
Angelica <s311322@studenti.polito.it>
2025-06-25 18:39:26 +0200

    lab12

--- UI/controller.py
+++ UI/controller.py
@@ -12,17 +12,56 @@ class Controller:
         self._listCountry = []
 
     def fillDD(self):
-        pass
+        self._listCountry = self._model.getAllNazioni()
+        for c in self._listCountry:
+            self._view.ddcountry.options.append(ft.dropdown.Option(c))
 
+        for i in range(2015, 2019):
+            self._listYear.append(i)
+        for anno in self._listYear:
+            self._view.ddyear.options.append(ft.dropdown.Option(anno))
 
     def handle_graph(self, e):
-        pass
-
+        nazione = self._view.ddcountry.value
+        anno = self._view.ddyear.value
+        self._model.buildGraph(nazione, anno)
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(ft.Text("Grafo creato correttamente"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero di vertici: {self._model.getNumNodi()}  Numero di archi: {self._model.getNumArchi()}"))
+        self._view.update_page()
 
 
     def handle_volume(self, e):
-        pass
+        volumi = self._model.getVolumi()
+        for v in volumi:
+            self._view.txtOut2.controls.append(ft.Text(f"{v[0]} --> {v[1]}"))
+        self._view.update_page()
 
 
     def handle_path(self, e):
-        pass
+        n = self._view.txtN.value
+        if n == "":
+            self._view.txtOut3.clean()
+            self._view.txtOut3.controls.append(ft.Text(f"Inserire la lunghezza del percorso desiderata."))
+
+        try:
+            nInt = int(n)
+        except ValueError:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("Inserisci un intero positivo."))
+            return
+
+        if nInt < 2:
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(ft.Text("Inserire un numero maggiore di 2."))
+            return
+
+        percorso, peso = self._model.getPercorso(nInt)
+        self._view.txtOut3.clean()
+        self._view.txtOut3.controls.append(ft.Text(f"Peso cammino massimo: {peso}"))
+
+        for i in range(0, len(percorso) - 1):
+            self._view.txtOut3.controls.append(ft.Text(
+                f"{percorso[i]} --> {percorso[i + 1]}: {self._model.getPesoArco(percorso[i], percorso[i + 1])}"))
+
+        self._view.update_page()
--- model/model.py
+++ model/model.py
@@ -1,3 +1,96 @@
+import copy
+
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self._graph = nx.Graph()
+        self._idMapRetailer = {}
+        self._pesoBest = 0
+        self._sol_Best = []
+
+    def getAllNazioni(self):
+        return DAO.getAllNazioni()
+
+    def buildGraph(self, nazione, anno):
+        self._graph.clear()
+        nodes = DAO.getAllNodes(nazione)
+
+        for n in nodes:
+            self._idMapRetailer[n.Retailer_code] = n
+
+        for n in nodes:
+            self._graph.add_node(n)
+
+        self.addAllArchi(anno, nazione)
+
+    def addAllArchi(self, anno, nazione):
+        archi = DAO.getAllArchi(anno, nazione, self._idMapRetailer)
+        for arco in archi:
+            a1 = arco[0]
+            a2 = arco[1]
+            self._graph.add_edge(a1, a2, weight=arco[2])
+
+    def getNumNodi(self):
+        return self._graph.number_of_nodes()
+
+    def getNumArchi(self):
+        return self._graph.number_of_edges()
+
+    def getVolumi(self):
+        lista_volumi = []
+        for nodo in self._graph.nodes:
+            vicini = self._graph.neighbors(nodo)
+            peso = 0
+            for v in vicini:
+                peso += self._graph[nodo][v]["weight"]
+            lista_volumi.append((nodo, peso))
+        #il peso è il secondo valore della tupla,
+        #io devo ordinarli in modo decrescente per peso
+        lista_volumi.sort(key=lambda x: x[1], reverse=True)
+        return lista_volumi
+
+
+    def _ricorsione(self,parziale, N):
+        #condizione terminale
+        if len(parziale)==N:
+            if self._graph.has_edge(parziale[-1],parziale[0]): #se dall'ultimo nodo posso ritornare al primo
+                ciclo = parziale + [parziale[0]]
+                peso = self.getPeso(ciclo)
+                if peso > self._pesoBest:
+                    self._pesoBest = peso
+                    self._sol_Best = copy.deepcopy(ciclo)
+                return
+
+        vicini = self._graph.neighbors(parziale[-1])
+        for v in vicini:
+            if v not in parziale and self._graph.has_edge(parziale[-1], v):
+                parziale.append(v)
+                self._ricorsione(parziale, N)
+                parziale.pop()
+
+
+    def getPercorso(self, N):
+       #Inizializza il miglior peso e la migliore soluzione
+        self._pesoBest = 0
+        self._sol_Best = []
+        parziale = []
+       #Per ogni nodo del grafo, prova ad avviare una ricerca ricorsiva a partire da quel nodo
+        for n in self._graph.nodes:
+            parziale.append(n)
+            self._ricorsione(parziale, N)
+            parziale.pop()
+        #Alla fine, restituisce il miglior ciclo trovato e il suo peso
+        return self._sol_Best, self._pesoBest
+
+    def getPesoArco(self, a, b):
+        return self._graph[a][b]["weight"]
+
+    def getPeso(self, parziale):
+        peso = 0
+        for i in range(0, len(parziale)-1):
+            peso += self._graph[parziale[i]][parziale[i+1]]["weight"]
+        return peso
